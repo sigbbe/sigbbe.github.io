1b93136c4f7dcb2eb936249f2d4728da334798ae
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2021-03-29 20:11:14 +0200

    Still messing around with ts to learn about types :3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import React, { FunctionComponent } from "react";
-import ParticleButton from "../components/particle-button/ParticleButton";
-
+import AnimationHeadingOne from '../components/my-heading/AnimationHeadingOne';
 import './App.sass';
 
 const App: FunctionComponent = () => {
     return (
         <div className={'Flex-center Flex-column Full-size'}>
-            <ParticleButton/>
+            <AnimationHeadingOne>
+                <h1>A</h1>
+                <h2>BB</h2>
+                <h3>CCC</h3>
+            </AnimationHeadingOne>
         </div>
     );
 };
