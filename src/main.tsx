3647aa55e79cd5edfbadb4b60f40d7e142626195
sigbjorn <sigbjorn.berdal14@gmail.com>
2022-08-27 03:32:56 +0200

    :rocket::rocket:

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import cv from "./assets/cv.pdf";
 import PdfFile from './utils/PdfFile';
@@ -9,12 +9,12 @@ import PdfFile from './utils/PdfFile';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<ChakraProvider>
-			<BrowserRouter>
+			<HashRouter>
 				<Routes>
 					<Route path="/" element={ <App /> } />
 					<Route path="cv" element={ <PdfFile file={ cv } /> } />
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</ChakraProvider>
 	</React.StrictMode>
 );
